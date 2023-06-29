efd6af4acdac5097daabda34b8bf365525a41b67
danivz <daniel.vazquez@upm.es>
2023-06-29 19:47:46 +0200

    Add VC709 board

---
+++ fpga/src/main/scala/vc709/Configs.scala
@@ -0,0 +1,74 @@
+package chipyard.fpga.vc709
+
+import sys.process._
+
+import org.chipsalliance.cde.config.{Config, Parameters}
+import freechips.rocketchip.subsystem.{SystemBusKey, PeripheryBusKey, ControlBusKey, ExtMem}
+import freechips.rocketchip.devices.debug.{DebugModuleKey, ExportDebug, JTAG}
+// import freechips.rocketchip.devices.tilelink.{DevNullParams, BootROMLocated}
+import freechips.rocketchip.diplomacy.{DTSModel, DTSTimebase, RegionType, AddressSet}
+import freechips.rocketchip.tile.{XLen}
+
+import sifive.blocks.devices.uart.{PeripheryUARTKey, UARTParams}
+
+import sifive.fpgashells.shell.{DesignKey}
+// import sifive.fpgashells.shell.xilinx.{VC7094GDDRSize}
+
+import testchipip.{SerialTLKey}
+
+import chipyard.{BuildSystem, ExtTLMem, DefaultClockFrequencyKey}
+
+class WithDefaultPeripherals extends Config((site, here, up) => {
+  case PeripheryUARTKey => List(UARTParams(address = BigInt(0x64000000L)))
+})
+
+class WithSystemModifications extends Config((site, here, up) => {
+  case DTSTimebase => BigInt{(1e6).toLong}
+  // case ExtMem => up(ExtMem, site).map(x => x.copy(master = x.master.copy(size = site(VC7094GDDRSize)))) // set extmem to DDR size (note the size)
+  case SerialTLKey => None // remove serialized tl port
+})
+
+class WithVC709Tweaks extends Config (
+  // harness binders
+  new WithVC709UARTHarnessBinder ++
+  new WithJTAGDebugBScan ++
+  // new WithVC709DDRMemHarnessBinder ++
+  // io binders
+  new WithUARTIOPassthrough ++
+  // new WithTLIOPassthrough ++
+  // other configuration
+  new WithDefaultPeripherals ++
+  // new chipyard.config.WithTLBackingMemory ++ // use TL backing memory
+  new WithSystemModifications ++ // setup busses, use sdboot bootrom, setup ext. mem. size
+  // new chipyard.config.WithNoDebug ++ // remove debug module
+  // new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+  // new freechips.rocketchip.subsystem.WithNMemoryChannels(1) ++
+  new WithFPGAFrequency(50) // default 50MHz freq
+)
+
+class TinyRocketVC709Config extends Config(
+  new WithVC709Tweaks ++
+  new freechips.rocketchip.subsystem.WithNBreakpoints(2) ++
+  new chipyard.TinyRocketConfig
+)
+
+class RocketVC709Config extends Config (
+  new WithVC709Tweaks ++
+  new chipyard.RocketConfig
+)
+
+class BoomVC709Config extends Config (
+  new WithFPGAFrequency(50) ++
+  new WithVC709Tweaks ++
+  new chipyard.MegaBoomConfig
+)
+
+class WithFPGAFrequency(fMHz: Double) extends Config (
+  new chipyard.config.WithPeripheryBusFrequency(fMHz) ++ // assumes using PBUS as default freq.
+  new chipyard.config.WithMemoryBusFrequency(fMHz)
+)
+
+class WithFPGAFreq25MHz extends WithFPGAFrequency(25)
+class WithFPGAFreq50MHz extends WithFPGAFrequency(50)
+class WithFPGAFreq75MHz extends WithFPGAFrequency(75)
+class WithFPGAFreq100MHz extends WithFPGAFrequency(100)
---
+++ fpga/src/main/scala/vc709/IOBinders.scala
@@ -0,0 +1,32 @@
+package chipyard.fpga.vc709
+
+import chisel3._
+import chisel3.experimental.{IO, DataMirror}
+
+import freechips.rocketchip.diplomacy.{ResourceBinding, Resource, ResourceAddress, InModuleBody}
+import freechips.rocketchip.subsystem.{BaseSubsystem}
+import freechips.rocketchip.util.{HeterogeneousBag}
+import freechips.rocketchip.tilelink.{TLBundle}
+
+import sifive.blocks.devices.uart.{HasPeripheryUARTModuleImp}
+
+import chipyard.{CanHaveMasterTLMemPort}
+import chipyard.iobinders.{OverrideIOBinder, OverrideLazyIOBinder}
+
+class WithUARTIOPassthrough extends OverrideIOBinder({
+  (system: HasPeripheryUARTModuleImp) => {
+    val io_uart_pins_temp = system.uart.zipWithIndex.map { case (dio, i) => IO(dio.cloneType).suggestName(s"uart_$i") }
+    (io_uart_pins_temp zip system.uart).map { case (io, sysio) =>
+      io <> sysio
+    }
+    (io_uart_pins_temp, Nil)
+  }
+})
+
+class WithTLIOPassthrough extends OverrideIOBinder({
+  (system: CanHaveMasterTLMemPort) => {
+    val io_tl_mem_pins_temp = IO(DataMirror.internal.chiselTypeClone[HeterogeneousBag[TLBundle]](system.mem_tl)).suggestName("tl_slave")
+    io_tl_mem_pins_temp <> system.mem_tl
+    (Seq(io_tl_mem_pins_temp), Nil)
+  }
+})
